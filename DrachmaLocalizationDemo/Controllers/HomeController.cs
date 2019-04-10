48c76b80465eb1a8a20649d6355512fa01752030
Arjun Sapkota <arjuns.sapkota@gmail.com>
2019-04-10 09:46:02 +0930

    init

---
+++ DrachmaLocalizationDemo/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DrachmaLocalizationDemo.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+
+namespace DrachmaLocalizationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStringLocalizer<HomeController> _localizer;
+        private readonly RequestLocalizationOptions _options;
+        
+        public HomeController(IStringLocalizer<HomeController> localizer,
+            RequestLocalizationOptions options)
+        {
+            _localizer = localizer;
+            _options = options;
+            
+        }
+
+        public IActionResult Index()
+        {
+            
+            var model = new CultureModel();
+            model.SupportedUICultures = new List<CultureInfo>(this._options.SupportedUICultures);
+            var localizedString = this._localizer["Greetings"];
+            ViewBag.Grettings = localizedString;
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+            {
+                return View();
+            }
+
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class CultureModel
+    {
+        public IReadOnlyList<CultureInfo> SupportedUICultures { get; set; }
+        
+    }
+
+    //public class SelzCultureProvider : RequestCultureProvider
+    //{
+    //    public override Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
+    //    {
+    //        if(httpContext==null)
+    //            throw new ArgumentNullException(nameof(httpContext));
+    //        httpContext.Request.QueryString.
+    //    }
+    //}
+
+
+}
---
+++ DrachmaLocalizationDemo/LocalizationExtension.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace DrachmaLocalizationDemo
+{
+    public static class LocalizationExtension
+    {
+        private static List<CultureInfo> SupportedCultures = new List<CultureInfo>
+        {
+            new CultureInfo("en-AU"),
+            new CultureInfo("fr-FR"),
+            new CultureInfo("ne-NP"),
+            new CultureInfo("ja-JP")
+        };
+
+        public static IServiceCollection AddSelzLocalization(this IServiceCollection coll)
+        {
+            coll.AddLocalization((a) =>
+            {
+                a.ResourcesPath = "Cultures";
+                
+            });
+            
+            
+            
+            coll.TryAddSingleton<RequestLocalizationOptions>(new RequestLocalizationOptions()
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = SupportedCultures,
+                SupportedUICultures = SupportedCultures,
+            });
+            return coll;
+        }
+
+        public static void UseSelzLocalization(this IApplicationBuilder app)
+        {
+            var fromDI = app.ApplicationServices.GetService<RequestLocalizationOptions>();
+            
+            //we can even customize this
+            //fromDI.RequestCultureProviders = null;
+
+            app.UseRequestLocalization(fromDI);
+
+        }
+
+
+    }
+}
